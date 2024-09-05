98b633838e93b1854e8d29a2536b140f0d39c3ab
Sven Lesche <113923517+SLesche@users.noreply.github.com>
2024-09-05 17:36:11 +0200

    statement set text

--- web-survey/functions/setupStatementSetSurvey.js
+++ web-survey/functions/setupStatementSetSurvey.js
@@ -3,18 +3,33 @@ function initializeStatementSetSurvey(control, statementset_idx) {
     const statementset_name = control.statementset_info[statementset_idx].statementset_name;
 
     document.getElementById("content").innerHTML = `
-    <h2>${statementset_name}</h2>
-    <p>Here some information about the statements that you used</p>
-    <form id="statementSetSurvey" class="survey-form">
-        <label for="statement_publication_file" class="survey-label">Please upload a .csv file where the statements you used are listed.</label>
-        <input type="file" id="statement_publication_file" name="statement_publication_file" accept=".csv" required><br>
-        <span id="file-name-display">${statementset_data.statement_publication_file ? `File: ${statementset_data.statement_publication_file.name}` : ''}</span><br>
+    <div class="display-text">
+        <h1>${statementset_name}</h1>
+        <p>In this section, please provide a file containing the statements used in your study. Including this file allows researchers to more closely replicate your study by reviewing the exact statements that were presented to participants.</p>
+        <p>It is important that the statement IDs in this file match exactly with those used in your raw data. This consistency ensures that the statements can be correctly linked to the corresponding data points in your dataset.</p>
+        <p>If available, please include columns with the following information:</p>
+        <ul>
+            <li><strong>statement_id:</strong> A unique identifier for each statement.</li>
+            <li><strong>statement_text:</strong> The text of the statement as it was presented to participants.</li>
+            <li><strong>statement_accuracy:</strong> The accuracy of the statement, indicating whether it is true or false.</li>
+            <li><strong>statement_category:</strong> The category or type of the statement, if applicable.</li>
+            <li><strong>proportion of people who rated it as true:</strong> The percentage of participants who rated the statement as true.</li>
+        </ul>
+        <p><i> HERE SHOULD GO AN EXAMPLE OF A CSV FILE</i></p>
+        <p>Including these details will enhance the reproducibility of your study and provide valuable context for those analyzing your data.</p>
+        <p>Once you have prepared your statement file according to these specifications, you can upload it using the form provided below. Thank you for your contribution!</p>
 
-        <label for="statement_publication" class="survey-label">If available, provide the original publication where this set of statements originates from.</label>
-        <input type="text" id="statement_publication" name="statement_publication" value="${statementset_data.statement_publication || ''}"><br>
+        <form id="statementSetSurvey" class="survey-form">
+            <label for="statement_publication_file" class="survey-label">Please upload a .csv file where the statements you used are listed.</label>
+            <input type="file" id="statement_publication_file" name="statement_publication_file" accept=".csv" required><br>
+            <span id="file-name-display">${statementset_data.statement_publication_file ? `File: ${statementset_data.statement_publication_file.name}` : ''}</span><br>
 
-        <button type="submit" class="survey-button">Submit</button>
-    </form>
+            <label for="statement_publication" class="survey-label">If available, provide the original publication where this set of statements originates from.</label>
+            <input type="text" id="statement_publication" name="statement_publication" value="${statementset_data.statement_publication || ''}"><br>
+
+            <button type="submit" class="survey-button">Submit</button>
+        </form>
+    </div>
     `;
 
     // Add event listener to the file input to display the selected file name
