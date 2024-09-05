7fc3eabd865dfe74c3ab671e8ddfc98869316d06
Sven Lesche <113923517+SLesche@users.noreply.github.com>
2024-09-05 16:43:49 +0200

    setup overview page

---
+++ web-survey/functions/setupOverviewPage.js
@@ -0,0 +1,13 @@
+function initializeOverviewPage(control) {
+    document.getElementById("content").innerHTML = `
+        <h2> Welcome!</h2>
+        <p>Hier kommt ein bisschen generischer Text zum Ausfüllen hin</p>
+        <p>Checklist: (hier dann Progress-Update über die Dinge geben</p>
+        <ul>
+            <li>Numbers update: Wie viele Studien / Datensätze sind schon drin?</li>
+            <li>Statementset updates</li>
+            <li>Checklist for raw data / Studien</li>
+        </ul>
+
+    `;
+}
