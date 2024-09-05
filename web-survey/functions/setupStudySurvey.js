f8fab2aefe6e7471862eee687ebe5ef4b1aee193
Sven Lesche <113923517+SLesche@users.noreply.github.com>
2024-09-05 17:20:59 +0200

    study description

--- web-survey/functions/setupStudySurvey.js
+++ web-survey/functions/setupStudySurvey.js
@@ -3,87 +3,91 @@ function initializeStudySurvey(control, publication_idx, study_idx) {
     studyName = control.publication_info[publication_idx].study_info[study_idx].study_name;
 
     document.getElementById("content").innerHTML = `
-        <h2>${studyName}</h2>
-        <p>Fill out the following survey to provide information about the study. A study is ...</p>
+    <div class="display-text">
+        <h1>${studyName}</h1>
+        <p>This section is designed to collect key details about the study you conducted. The information you provide here will help us better understand the scope and methodology of your research. You'll be asked about how you measured truth ratings, what other measures were included in your study, the types of analysis you performed, and the specific statements or stimuli used in your research.</p>
+        <p>If your study is available on an open-source platform, you’ll also have the option to share a link, ensuring that others can access the full study for further exploration.</p>
+        <p>This information is crucial for ensuring that your study is well-documented and can be effectively integrated into our database. Thank you for taking the time to provide these details.</p>
+
         <form id="studySurvey" class="survey-form">
-        <label for="truth_rating_scale" class="survey-label">With what scale did you measure the truth rating of a statement?</label>
-        <div class="radio-buttons">
-            <input type="radio" id="dichotomous" name="truth_rating_scale" value="dichotomous" ${study_data.truth_rating_scale === 'dichotomous' ? 'checked' : ''} required>
-            <label for="dichotomous">Dichotomous</label>
-            <input type="radio" id="likert" name="truth_rating_scale" value="likert" ${study_data.truth_rating_scale === 'likert' ? 'checked' : ''} required>
-            <label for="likert">Likert</label>
-            <input type="radio" id="range" name="truth_rating_scale" value="range" ${study_data.truth_rating_scale === 'range' ? 'checked' : ''} required>
-            <label for="range">Range</label>
-            <input type="radio" id="other" name="truth_rating_scale" value="other" ${study_data.truth_rating_scale === 'other' ? 'checked' : ''} required>
-            <label for="other">Other</label><br>
-        </div>
-        <fieldset id="other_rating_scale_fieldset" ${study_data.truth_rating_scale === 'other' ? '' : 'disabled'}>
-            <div class="form-item">
-                <label for="truth_rating_scale_details" class="survey-label">Please provide further details:</label>
-                <input type="text" id="truth_rating_scale_details" name="truth_rating_scale_details" value="${study_data.truth_rating_scale_details || ''}" required/>
+            <label for="truth_rating_scale" class="survey-label">With what scale did you measure the truth rating of a statement?</label>
+            <div class="radio-buttons">
+                <input type="radio" id="dichotomous" name="truth_rating_scale" value="dichotomous" ${study_data.truth_rating_scale === 'dichotomous' ? 'checked' : ''} required>
+                <label for="dichotomous">Dichotomous</label>
+                <input type="radio" id="likert" name="truth_rating_scale" value="likert" ${study_data.truth_rating_scale === 'likert' ? 'checked' : ''} required>
+                <label for="likert">Likert</label>
+                <input type="radio" id="range" name="truth_rating_scale" value="range" ${study_data.truth_rating_scale === 'range' ? 'checked' : ''} required>
+                <label for="range">Range</label>
+                <input type="radio" id="other" name="truth_rating_scale" value="other" ${study_data.truth_rating_scale === 'other' ? 'checked' : ''} required>
+                <label for="other">Other</label><br>
             </div>
-        </fieldset>
+            <fieldset id="other_rating_scale_fieldset" ${study_data.truth_rating_scale === 'other' ? '' : 'disabled'}>
+                <div class="form-item">
+                    <label for="truth_rating_scale_details" class="survey-label">Please provide further details:</label>
+                    <input type="text" id="truth_rating_scale_details" name="truth_rating_scale_details" value="${study_data.truth_rating_scale_details || ''}" required/>
+                </div>
+            </fieldset>
 
-        <label for="truth_rating_steps" class="survey-label">How many steps did your rating scale have?</label>
-        <input type="number" id="truth_rating_steps" name="truth_rating_steps" value="${study_data.truth_rating_steps || ''}" required><br>
-        
-        <label for="statement_set_select" class="survey-label">Select the statement set used:</label>
-        <select id="statement_set_select" name="statement_set_select" required>
-            <option value="">Select a statement set</option>
-            <!-- Options will be populated dynamically -->
-        </select><br>
+            <label for="truth_rating_steps" class="survey-label">How many steps did your rating scale have?</label>
+            <input type="number" id="truth_rating_steps" name="truth_rating_steps" value="${study_data.truth_rating_steps || ''}" required><br>
+            
+            <label for="statement_set_select" class="survey-label">Select the statement set used:</label>
+            <select id="statement_set_select" name="statement_set_select" required>
+                <option value="">Select a statement set</option>
+                <!-- Options will be populated dynamically -->
+            </select><br>
 
-        <label for="subjective_certainty" class="survey-label">Was subjective certainty measured?</label>
-        <div class="radio-buttons">
-            <label><input type="radio" name="subjective_certainty" value="1" ${study_data.subjective_certainty == 1 ? 'checked' : ''} required/>Yes</label>
-            <label><input type="radio" name="subjective_certainty" value="0" ${study_data.subjective_certainty == 0 ? 'checked' : ''} required/>No</label>
-        </div>
+            <label for="subjective_certainty" class="survey-label">Was subjective certainty measured?</label>
+            <div class="radio-buttons">
+                <label><input type="radio" name="subjective_certainty" value="1" ${study_data.subjective_certainty == 1 ? 'checked' : ''} required/>Yes</label>
+                <label><input type="radio" name="subjective_certainty" value="0" ${study_data.subjective_certainty == 0 ? 'checked' : ''} required/>No</label>
+            </div>
 
-        <label for="rt_measured" class="survey-label">Did you measure response time?</label>
-        <div class="radio-buttons">
-            <label><input type="radio" name="rt_measured" value="1" ${study_data.rt_measured == 1 ? 'checked' : ''} required/>Yes</label>
-            <label><input type="radio" name="rt_measured" value="0" ${study_data.rt_measured == 0 ? 'checked' : ''} required/>No</label>
-        </div>
+            <label for="rt_measured" class="survey-label">Did you measure response time?</label>
+            <div class="radio-buttons">
+                <label><input type="radio" name="rt_measured" value="1" ${study_data.rt_measured == 1 ? 'checked' : ''} required/>Yes</label>
+                <label><input type="radio" name="rt_measured" value="0" ${study_data.rt_measured == 0 ? 'checked' : ''} required/>No</label>
+            </div>
 
-        <label for="rt_onset" class="survey-label">What event marked the onset of response time measurement?</label>
-        <input type="text" id="rt_onset" name="rt_onset" value="${study_data.rt_onset || ''}" required><br>
+            <label for="rt_onset" class="survey-label">What event marked the onset of response time measurement?</label>
+            <input type="text" id="rt_onset" name="rt_onset" value="${study_data.rt_onset || ''}" required><br>
 
-        <label for="n_groups" class="survey-label">How many between conditions did you have in the study?</label>
-        <input type="number" id="n_groups" name="n_groups" value="${study_data.n_groups || ''}" required><br>
+            <label for="n_groups" class="survey-label">How many between conditions did you have in the study?</label>
+            <input type="number" id="n_groups" name="n_groups" value="${study_data.n_groups || ''}" required><br>
 
-        <label for="participant_age" class="survey-label">Was was the average age of your participants?</label>
-        <input type="number" step="0.01" id="participant_age" name="participant_age" value="${study_data.participant_age || ''}" required><br>
+            <label for="participant_age" class="survey-label">Was was the average age of your participants?</label>
+            <input type="number" step="0.01" id="participant_age" name="participant_age" value="${study_data.participant_age || ''}" required><br>
 
-        <label for="percentage_female" class="survey-label">Which percentage of your participants was female?</label>
-        <input type="number" step="0.01" id="percentage_female" name="percentage_female" value="${study_data.percentage_female || ''}" required><br>
+            <label for="percentage_female" class="survey-label">Which percentage of your participants was female?</label>
+            <input type="number" step="0.01" id="percentage_female" name="percentage_female" value="${study_data.percentage_female || ''}" required><br>
 
-        <label for="external_vars" class="survey-label">Please list any external variables you measured.</label>
-        <input type="text" id="external_vars" name="external_vars" value ="${study_data.external_vars || ''}" required><br>
+            <label for="external_vars" class="survey-label">Please list any external variables you measured.</label>
+            <input type="text" id="external_vars" name="external_vars" value ="${study_data.external_vars || ''}" required><br>
 
-        <label for="physiological_measures" class="survey-label">Did you collect any physiological data?</label>
-        <div class="radio-buttons">
-            <label><input type="radio" name="physiological_measures" value="1" ${study_data.physiological_measures == 1 ? 'checked' : ''} required/>Yes</label>
-            <label><input type="radio" name="physiological_measures" value="0" ${study_data.physiological_measures == 0 ? 'checked' : ''} required/>No</label>
-        </div>
+            <label for="physiological_measures" class="survey-label">Did you collect any physiological data?</label>
+            <div class="radio-buttons">
+                <label><input type="radio" name="physiological_measures" value="1" ${study_data.physiological_measures == 1 ? 'checked' : ''} required/>Yes</label>
+                <label><input type="radio" name="physiological_measures" value="0" ${study_data.physiological_measures == 0 ? 'checked' : ''} required/>No</label>
+            </div>
 
-        <label for="cognitive_models" class="survey-label">Did you employ any cognitive models in your analysis?</label>
-        <div class="radio-buttons">
-            <label><input type="radio" name="cognitive_models" value="1" ${study_data.cognitive_models == 1 ? 'checked' : ''} required/>Yes</label>
-            <label><input type="radio" name="cognitive_models" value="0" ${study_data.cognitive_models == 0 ? 'checked' : ''} required/>No</label>
-        </div>
+            <label for="cognitive_models" class="survey-label">Did you employ any cognitive models in your analysis?</label>
+            <div class="radio-buttons">
+                <label><input type="radio" name="cognitive_models" value="1" ${study_data.cognitive_models == 1 ? 'checked' : ''} required/>Yes</label>
+                <label><input type="radio" name="cognitive_models" value="0" ${study_data.cognitive_models == 0 ? 'checked' : ''} required/>No</label>
+            </div>
 
-        <label for="github" class="survey-label">If available, provide the link to the data on GitHub.</label>
-        <input type="text" id="github" name="github" value="${study_data.github || ''}" required><br>
+            <label for="github" class="survey-label">If available, provide the link to the data on GitHub.</label>
+            <input type="text" id="github" name="github" value="${study_data.github || ''}" required><br>
 
-        <label for="osf" class="survey-label">If available, provide the link to the data on OSF.</label>
-        <input type="text" id="osf" name="osf" value="${study_data.osf || ''}" required><br>
+            <label for="osf" class="survey-label">If available, provide the link to the data on OSF.</label>
+            <input type="text" id="osf" name="osf" value="${study_data.osf || ''}" required><br>
 
-        <label for="study_comment" class="survey-label">Would you like to provide any additional information?</label>
-        <textarea id="study_comment" name="study_comment" rows="4" cols="50">${study_data.study_comment || ''}</textarea><br>
+            <label for="study_comment" class="survey-label">Would you like to provide any additional information?</label>
+            <textarea id="study_comment" name="study_comment" rows="4" cols="50">${study_data.study_comment || ''}</textarea><br>
 
-        <button type="submit" class="survey-button">Submit</button>
-    </form>
-
+            <button type="submit" class="survey-button">Submit</button>
+        </form>
+    </div>
     `;
 
     populateStatementSets(control);
