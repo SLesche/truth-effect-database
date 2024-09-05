cee6f3b5c0a521d936f2adb423611631e5993185
Sven Lesche <113923517+SLesche@users.noreply.github.com>
2024-09-05 17:24:22 +0200

    measurement survey display

--- web-survey/functions/setupMeasurementSurvey.js
+++ web-survey/functions/setupMeasurementSurvey.js
@@ -1,10 +1,13 @@
 function initializeMeasurementSurvey(control, publication_idx, study_idx){
     measurement_data = control.publication_info[publication_idx].study_info[study_idx].measurement_info.data;
-    measurement_name = "Additional Measures"
 
     document.getElementById("content").innerHTML = `
-        <h2>${measurement_name}</h2>
-        <p> here we want to know what additional variables you measured in the study</p>
+    <div class = "display-text">
+        <h1>Additional Measurements</h1>
+        <p>In this section, we want to know if you collected any additional measurements beyond the primary variables of your study. This information is valuable for helping others identify datasets that include external variables they may be interested in.</p>
+        <p>To make the data more searchable and easier to navigate, we encourage you to use broad construct terms, such as "extraversion," "intelligence," or "anxiety," rather than specific test batteries or questionnaires. This ensures that others can quickly find relevant data based on common constructs rather than being limited by specific measurement tools.</p>
+        <p>By providing this information, you contribute to a more comprehensive and accessible dataset, enabling others to explore connections between truth ratings and various other factors.</p>
+
         <form id="measurementSurvey">
             <label for="measureInput" class="survey-label">Add any additional variables you measured in the study:</label>
             <input type="text" id="measureInput" name="measureInput"><br>
@@ -16,6 +19,7 @@ function initializeMeasurementSurvey(control, publication_idx, study_idx){
 
             <button type="submit" class="survey-button">Submit</button>
         </form>
+    </div>
     `;
 
     // Display previous submission if available
