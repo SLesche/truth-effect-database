ef36416b52a29effc60ecd7684f91a0302eab862
Sven Lesche <113923517+SLesche@users.noreply.github.com>
2024-09-24 16:50:53 +0200

    start json extraction process

--- example_submission_data/reading_submissions.R
+++ example_submission_data/reading_submissions.R
@@ -1,11 +1,11 @@
 library(jsonlite)
 library(tidyverse)
 
-path <- "example_submission_data/submissions/"
+path <- "example_submission_data/"
 
-file <- read_json(paste0(path, "submission_test_jalbert.json"))
+file <- read_json(paste0(path, "submission_test.json"))
 
-raw_data_1 <- file$publication_info$`0`$study_info$`0`$raw_data$data
+raw_data_1 <- file$study_info$`0`$raw_data
 
 
 raw_data_1_df <- data.table::rbindlist(raw_data_1)
--- functions/extract_from_submission_json.R
+++ functions/extract_from_submission_json.R
@@ -3,22 +3,44 @@ extract_from_submission_json <- function(json_path){
   
   submission_list = list()
   
-  submission_list$publication_data = as.data.frame(json_obj$publication_info[[1]]$publication_data)
+  submission_list$publication_data = as.data.frame(json_obj$publication_data)
   
   n_statementsets = length(json_obj$statementset_info)
   
   if (n_statementsets > 0) {
     for (istatementset in 1:n_statementsets){
       submission_list$statementset_info[[istatementset]]$publication = json_obj$statementset_info[[istatementset]]$statementset_data$statement_publication
-      submission_list$statementset_info[[istatementset]]$statementset_data = data.table::rbindlist(json_obj$statementset_info[[istatementset]]$statementset_data$statement_publication_data)
+      submission_list$statementset_info[[istatementset]]$statementset_data = data.table::rbindlist(json_obj$statementset_info[[istatementset]]$statementset_data)
     }
   } else {
     # Do something else?
   }
   
-  n_studies = length(json_obj$publication_info[[1]]$study_info)
+  n_studies = length(json_obj$study_info)
   
   for (istudy in 1:n_studies){
-    submission_list$study_info[[istudy]]$study_data = submission_list
+    submission_list$study_info[[istudy]]$study_data = as.data.frame(json_obj$study_info[[istudy]]$study_data)
+    submission_list$study_info[[istudy]]$repetition_data = data.table::rbindlist(json_obj$study_info[[istudy]]$repetition_data)
+    
+    # Here check if has conditions
+    if (json_obj$study_info[[istudy]]$condition_data$has_within_conditions == "1"){
+      submission_list$study_info[[istudy]]$within_data
+    } else {
+      # Do something else?
+    }
+    
+    if (json_obj$study_info[[istudy]]$condition_data$has_between_conditions == "1"){
+      submission_list$study_info[[istudy]]$between_data
+    } else {
+      # Do something else?
+    }
+    
+    # Here extract only necessary columns from the raw data
+    submission_list$study_info[[istudy]]$raw_data = data.table::rbindlist(json_obj$study_info[[istudy]]$raw_data)
+    
+    # Deal with submitted additional measures
+    if (json_obj$study_info[[istudy]]$measurement_data$additional_measures == "1"){
+      submission_list$study_info[[istudy]]$measurement_data = data.table::rbindlist(json_obj$study_info[[istudy]]$measurement_data)
+    }
   }
 }
