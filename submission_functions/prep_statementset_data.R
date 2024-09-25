8c21fe857254a1426a451bf22a24a4da1510632e
Sven Lesche <113923517+SLesche@users.noreply.github.com>
2024-09-25 18:45:34 +0200

    Update prep_statementset_data.R

--- submission_functions/prep_statementset_data.R
+++ submission_functions/prep_statementset_data.R
@@ -1,4 +1,19 @@
-prep_statementset_data <- function(statementset_data){
+prep_statementset_data <- function(statementset_data, db_overview){
+  possible_columns = db_overview[db_overview$table == "statement_table", "column_name"]
   
-  return (statementset_data)
+  possible_columns = gsub("_id$", "_identifier", possible_columns)
+  
+  clean_statementset_data = dplyr::select(statementset_data, dplyr::any_of(possible_columns))
+  
+  numeric_columns = db_overview |>
+    dplyr::filter(table == "statement_table") |>
+    dplyr::filter(data_type %in% c("BOOLEAN", "INTEGER", "FLOAT")) |>
+    dplyr::pull(column_name)
+  
+  clean_statementset_data = dplyr::mutate(
+    clean_statementset_data,
+    dplyr::across(dplyr::any_of(numeric_columns), ~as.numeric(sub(",", ".", ., fixed = TRUE)))
+    )
+  
+  return (clean_statementset_data)
 }
