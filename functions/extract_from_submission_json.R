4fd7ac0831c43c4d6f518180556d5a1850079f88
Sven Lesche <113923517+SLesche@users.noreply.github.com>
2024-09-25 10:49:47 +0200

    json test condition extraction

--- functions/extract_from_submission_json.R
+++ functions/extract_from_submission_json.R
@@ -1,5 +1,5 @@
 extract_from_submission_json <- function(json_path){
-  json_obj = jsonlite::read_json(paste0(path, "submission_test_jalbert.json"))
+  json_obj = jsonlite::read_json(json_path)
   
   submission_list = list()
   
@@ -12,8 +12,6 @@ extract_from_submission_json <- function(json_path){
       submission_list$statementset_info[[istatementset]]$publication = json_obj$statementset_info[[istatementset]]$statementset_data$statement_publication
       submission_list$statementset_info[[istatementset]]$statementset_data = data.table::rbindlist(json_obj$statementset_info[[istatementset]]$statementset_data)
     }
-  } else {
-    # Do something else?
   }
   
   n_studies = length(json_obj$study_info)
@@ -22,25 +20,45 @@ extract_from_submission_json <- function(json_path){
     submission_list$study_info[[istudy]]$study_data = as.data.frame(json_obj$study_info[[istudy]]$study_data)
     submission_list$study_info[[istudy]]$repetition_data = data.table::rbindlist(json_obj$study_info[[istudy]]$repetition_data)
     
+    columns_to_extract = c("subject", "session", "trial", "response", "repeated")
+    
     # Here check if has conditions
     if (json_obj$study_info[[istudy]]$condition_data$has_within_conditions == "1"){
-      submission_list$study_info[[istudy]]$within_data
-    } else {
-      # Do something else?
+      submission_list$study_info[[istudy]]$within_data = data.table::rbindlist(json_obj$study_info[[istudy]]$condition_data$within_condition_details)
+      columns_to_extract = c(columns_to_extract, "within_identifier")
     }
     
     if (json_obj$study_info[[istudy]]$condition_data$has_between_conditions == "1"){
-      submission_list$study_info[[istudy]]$between_data
-    } else {
-      # Do something else?
+      submission_list$study_info[[istudy]]$between_data = data.table::rbindlist(json_obj$study_info[[istudy]]$condition_data$between_condition_details)
+      columns_to_extract = c(columns_to_extract, "between_identifier")
     }
     
     # Here extract only necessary columns from the raw data
     submission_list$study_info[[istudy]]$raw_data = data.table::rbindlist(json_obj$study_info[[istudy]]$raw_data)
     
+    # Add columns to extract
+    if (json_obj$study_info[[istudy]]$study_data$statementset_name != "no information"){
+      columns_to_extract = c(columns_to_extract, "statement_identifier")
+    }
+    
+    if (json_obj$study_info[[istudy]]$study_data$rt_measured == "1"){
+      columns_to_extract = c(columns_to_extract, "rt")
+    }
+    
+    if (json_obj$study_info[[istudy]]$study_data$subjective_certainty == "1"){
+      columns_to_extract = c(columns_to_extract, "certainty")
+    }
+    
+    submission_list$study_info[[istudy]]$raw_data = as.data.frame(submission_list$study_info[[istudy]]$raw_data)[, columns_to_extract]
+    
+    # Maximum-normalize the response column
+    submission_list$study_info[[istudy]]$raw_data$response = maximum_normalize(submission_list$study_info[[istudy]]$raw_data$response)
+    
     # Deal with submitted additional measures
     if (json_obj$study_info[[istudy]]$measurement_data$additional_measures == "1"){
-      submission_list$study_info[[istudy]]$measurement_data = data.table::rbindlist(json_obj$study_info[[istudy]]$measurement_data)
+      submission_list$study_info[[istudy]]$measurement_data = data.table::rbindlist(json_obj$study_info[[istudy]]$measurement_data$measures)
     }
   }
+  
+  return(submission_list)
 }
