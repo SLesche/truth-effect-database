9d7a61ecb44e8baf91601fe9901b4eebfe40ae79
Sven Lesche <113923517+SLesche@users.noreply.github.com>
2024-09-26 10:57:00 +0200

    provide testing script

--- example_submission_data/reading_submissions.R
+++ example_submission_data/reading_submissions.R
@@ -27,11 +27,30 @@ arguments <- list() %>%
     "proportion_true",
     "greater",
     "0.5"
+  ) %>% 
+  add_argument(
+    conn,
+    "subjective_certainty",
+    "equal",
+    "0"
+  ) %>% 
+  add_argument(
+    conn,
+    "repetition_time",
+    "equal",
+    "20"
   )
+
+db_overview = generate_db_overview_table(db_path)
+
+target_cols <- db_overview %>% 
+  filter(table %in% c("observation_table", "within_table", "between_table", "repetition_table", "statement_table", "statementset_table", "study_table", "publication_table")) %>% 
+  pull(column_name) %>% 
+  unique()
 
 result <- query_db(conn,
                    arguments,
-                   target_vars = c("default"),
-                   target_table = "study_table")
+                   target_vars = target_cols,
+                   target_table = "observation_table")
 
 result %>% distinct() %>% View()
