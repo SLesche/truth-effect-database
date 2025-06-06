9650b3942971d066040548ac3c9760ba43efb347
Sven Lesche <113923517+SLesche@users.noreply.github.com>
2025-06-06 11:35:08 +0200

    submit more data

--- example_submission_data/prep_submissions/corneille2020repetition/prep_data.R
+++ example_submission_data/prep_submissions/corneille2020repetition/prep_data.R
@@ -2,38 +2,110 @@ library(tidyverse)
 
 script_dir <- dirname(rstudioapi::getSourceEditorContext()$path)
 
-data <- data.table::fread(paste0(script_dir, "./data/data_extendexp1.csv"))
+data <- data.table::fread(paste0(script_dir, "./data/fabr_raw.csv"))
 
 statement_data <- data %>% 
-  select(enonce,
-         enonce_verite) %>% 
+  select(statement_id,
+         statements,
+         truth) %>% 
+  distinct() %>% 
+  mutate(
+    statement_identifier = statement_id,
+    statement_text = statements,
+    statement_accuracy = ifelse(truth, 1, 0)
+  )
+
+write.csv(statement_data, paste0(script_dir, "./data/statement_data_1.csv"))
+
+clean_data <- data %>% 
+  left_join(statement_data) %>% 
+  mutate(
+    phase = ifelse(response == "", "exposure", "test")
+  )
+
+old_statements <- clean_data %>% 
+  filter(phase == "exposure") %>% 
+  distinct(componentResultId, statement_id) %>% 
+  mutate(
+    repeated = 1
+  )
+
+clean_data <- clean_data %>% 
+  left_join(old_statements) %>% 
+  mutate(
+    subject = componentResultId,
+    presentation_identifier = phase,
+    trial = NA,
+    within_identifier = 1,
+    between_identifier = 1,
+    rt = response_time / 1000,
+    response = ifelse(response == "y", 1, 0),
+    repeated = ifelse(repeated == 1, 1, 0)
+  ) %>% 
+  select(subject, ends_with("identifier"), rt, response, repeated, trial)
+
+write.csv(clean_data, paste0(script_dir, "./data/clean_data_1.csv"))
+
+#### Exp 2 ----
+
+data <- data.table::fread(paste0(script_dir, "./data/data_fakeness.csv"))
+
+statement_data <- data %>% 
+  select(statement,
+         truth) %>% 
   distinct() %>% 
   mutate(
     statement_identifier = row_number(),
-    statement_text = enonce,
-    statement_accuracy = ifelse(enonce_verite == "t", 1, 0)
+    statement_text = statement,
+    statement_accuracy = ifelse(truth, 1, 0)
   )
 
-write.csv(statement_data, paste0(script_dir, "./data/statement_data.csv"))
+write.csv(statement_data, paste0(script_dir, "./data/statement_data_2.csv"))
 
 clean_data <- data %>% 
   left_join(statement_data) %>% 
   mutate(
     subject = ppt,
     presentation_identifier = 1,
-    trial = NA,
+    trial = pos_judgment_phase,
     within_identifier = 1,
-    between_identifier = case_when(
-      manip_attention == "nd" & manip_cred == "nc" ~ "noatt_nocred",
-      manip_attention == "d" & manip_cred == "nc" ~ "att_nocred",
-      manip_attention == "nd" & manip_cred == "c" ~ "noatt_cred",
-      manip_attention == "d" & manip_cred == "c" ~ "att_cred",
-      
-    ),
-    rt = truth_time / 1000,
-    response = ifelse(truth_rating == "m", 1, 0),
-    repeated = ifelse(enonce_repetition == "r", 1, 0)
+    between_identifier = 1,
+    rt = NA,
+    response = ifelse(response == "resp_yes", 1, 0),
+    repeated = ifelse(repetition == "repeated", 1, 0)
   ) %>% 
   select(subject, ends_with("identifier"), rt, response, repeated, trial)
 
-write.csv(clean_data, paste0(script_dir, "./data/clean_data.csv"))
+write.csv(clean_data, paste0(script_dir, "./data/clean_data_2.csv"))
+
+
+#### Exp 3 ----
+data <- data.table::fread(paste0(script_dir, "./data/data_truth_vs_falsehood.csv"))
+
+statement_data <- data %>% 
+  select(statement,
+         truth) %>% 
+  distinct() %>% 
+  mutate(
+    statement_identifier = row_number(),
+    statement_text = statement,
+    statement_accuracy = ifelse(truth, 1, 0)
+  )
+
+write.csv(statement_data, paste0(script_dir, "./data/statement_data_3.csv"))
+
+clean_data <- data %>% 
+  left_join(statement_data) %>% 
+  mutate(
+    subject = ppt,
+    presentation_identifier = 1,
+    trial = pos_judgment_phase,
+    within_identifier = 1,
+    between_identifier = instructions,
+    rt = NA,
+    response = ifelse(response == "resp_yes", 1, 0),
+    repeated = ifelse(repetition == "repeated", 1, 0)
+  ) %>% 
+  select(subject, ends_with("identifier"), rt, response, repeated, trial)
+
+write.csv(clean_data, paste0(script_dir, "./data/clean_data_3.csv"))
---
+++ example_submission_data/prep_submissions/ecker2020corrections/prep_data.R
@@ -0,0 +1,96 @@
+library(tidyverse)
+
+script_dir <- dirname(rstudioapi::getSourceEditorContext()$path)
+
+data <- data.table::fread(paste0(script_dir, "./data/data_E1.csv"))
+
+clean_data <- data %>% 
+  select(Condition, ends_with("_IQ")) %>% 
+  mutate(subject = row_number()) %>% 
+  pivot_longer(
+    cols = -c(Condition, subject),
+    names_to = "type",
+    values_to = "rating"
+  ) %>% 
+  mutate(
+    repeated = ifelse(Condition == "NE", 0, 1)
+  ) %>% 
+  mutate(
+    presentation_identifier = 1,
+    statement_identifier = type,
+    trial = NA,
+    rt = NA,
+    within_identifier = ifelse(str_detect(type, "^M"), "myth", "fact"),
+    between_identifier = Condition,
+    response = rating,
+  )
+
+statement_data <- clean_data %>% 
+  distinct(type) %>% 
+  mutate(
+    statement_identifier = type,
+    statement_text = NA,
+    statement_accuracy = ifelse(str_detect(type, "^M"), 0, 1)
+  )
+write.csv(statement_data, paste0(script_dir, "./data/statement_data.csv"))
+
+clean_data <- clean_data %>% 
+  select(subject, ends_with("identifier"), rt, response, repeated, trial)
+
+write.csv(clean_data, paste0(script_dir, "./data/clean_data_1.csv"))
+
+### Exp 2 ----
+data <- data.table::fread(paste0(script_dir, "./data/data_E2.csv"))
+
+clean_data <- data %>% 
+  select(Condition, ends_with("_BR")) %>% 
+  mutate(subject = row_number()) %>% 
+  pivot_longer(
+    cols = -c(Condition, subject),
+    names_to = "type",
+    values_to = "rating"
+  ) %>% 
+  mutate(
+    repeated = ifelse(Condition == "NE", 0, 1)
+  ) %>% 
+  mutate(
+    presentation_identifier = ifelse(str_detect(Condition, "D$"), "delayed", "immediate"),
+    trial = NA,
+    rt = NA,
+    statement_identifier = type,
+    within_identifier = ifelse(str_detect(type, "^M"), "myth", "fact"),
+    between_identifier = str_remove(Condition, "D|I$"),
+    response = rating,
+  ) %>% 
+  select(subject, ends_with("identifier"), rt, response, repeated, trial)
+
+write.csv(clean_data, paste0(script_dir, "./data/clean_data_2.csv"))
+
+### Exp 3 ----
+data <- data.table::fread(paste0(script_dir, "./data/data_E3.csv"))
+
+clean_data <- data %>% 
+  select(Cond, ends_with("_BR")) %>% 
+  mutate(subject = row_number()) %>% 
+  pivot_longer(
+    cols = -c(Cond, subject),
+    names_to = "type",
+    values_to = "rating"
+  ) %>% 
+  mutate(
+    repeated = ifelse(Cond == "NE", 0, 1)
+  ) %>% 
+  mutate(
+    presentation_identifier = 1,
+    trial = NA,
+    rt = NA,
+    within_identifier = ifelse(str_detect(type, "^M"), "myth", "fact"),
+    between_identifier = Cond,
+    response = rating,
+    statement_identifier = type,
+  ) %>% 
+  select(subject, ends_with("identifier"), rt, response, repeated, trial)
+
+write.csv(clean_data, paste0(script_dir, "./data/clean_data_3.csv"))
+
+
