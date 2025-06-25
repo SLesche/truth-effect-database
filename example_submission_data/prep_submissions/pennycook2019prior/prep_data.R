166f92a954f5b52c93c193c915cfd34b436ef6ba
Sven Lesche <113923517+SLesche@users.noreply.github.com>
2025-06-25 18:00:51 +0200

    pennycook update

--- example_submission_data/prep_submissions/pennycook2019prior/prep_data.R
+++ example_submission_data/prep_submissions/pennycook2019prior/prep_data.R
@@ -75,9 +75,9 @@ clean_data <- data %>%
   ) %>% 
   filter(type != "rt") %>% 
   mutate(
-    presentation_identifier = 1,
+    presentation_identifier = Warning,
     trial = NA,
-    between_identifier = Warning,
+    between_identifier = 1,
     repeated = case_when(
       Counterbalance == 1 & statement_number < 7 ~ 1,
       Counterbalance == 1 & statement_number > 6 ~ 0,
@@ -120,28 +120,24 @@ clean_session1_data <-  session1_data %>%
     statement_number = as.numeric(str_extract(item, "\\d+")),
   ) %>% 
   mutate(
-    presentation_identifier = 1,
+    presentation_identifier = paste0(Warning, "_session_", 1),
     trial = NA,
     between_identifier = 1,
     repeated = case_when(
-      Condition %in% c(1) & statement_number %in% c(1:4) ~ 1,
-      Condition %in% c(1) & statement_number %in% c(5:8) ~ 0,
-      Condition %in% c(1) & statement_number %in% c(1:4) ~ 1,
-      Condition %in% c(1) & statement_number %in% c(5:8) ~ 0,
+      Condition %in% c(1, 2, 7, 8) & statement_number %in% c(1:4) ~ 1,
+      Condition %in% c(3, 4, 9, 10) & statement_number %in% c(5:8) ~ 1,
+      Condition %in% c(5, 6, 11, 12) & statement_number %in% c(9:12) ~ 1,
+      Condition %in% c(3, 5, 9, 11) & statement_number %in% c(1:4) ~ 0,
+      Condition %in% c(1, 6, 7, 12) & statement_number %in% c(5:8) ~ 0,
+      Condition %in% c(2, 4, 8, 10) & statement_number %in% c(9:12) ~ 0,
+      TRUE ~ NA
     )
-  )
-
-clean_session1_data %>% 
-  count(Condition, statement_number, is.na(response)) %>% View()
-  
-
-clean_session1_data %>% 
-  group_by(
-    statement_accuracy
   ) %>% 
-  summarize(mean_resp = mean(response, na.rm = TRUE))
-  
-clean_data <- session2_data %>% 
+  filter(!is.na(repeated)) %>%
+  select(subject, ends_with("identifier"), contains("statement"), response, repeated, trial)
+
+clean_session2_data <- session2_data %>%
+  filter(!is.na(Condition)) %>% 
   select(Warning, Condition, matches("(Real|Fake)\\d+_2$")) %>% 
   mutate(subject = row_number()) %>% 
   pivot_longer(
@@ -159,74 +155,31 @@ clean_data <- session2_data %>%
     statement_number = as.numeric(str_extract(item, "\\d+")),
   ) %>% 
   mutate(
-    presentation_identifier = 1,
+    presentation_identifier = paste0(Warning, "_session_", 2),
     trial = NA,
     between_identifier = 1,
     repeated = case_when(
-      Condition %in% c(1) & statement_number %in% c(1:4) ~ 1,
-      Condition %in% c(1) & statement_number %in% c(5:8) ~ 0,
-      Condition %in% c(1) & statement_number %in% c(1:4) ~ 1,
-      Condition %in% c(1) & statement_number %in% c(5:8) ~ 0,
+      Condition %in% c(1, 2, 7, 8) & statement_number %in% c(1:4) ~ 1,
+      Condition %in% c(3, 4, 9, 10) & statement_number %in% c(5:8) ~ 1,
+      Condition %in% c(5, 6, 11, 12) & statement_number %in% c(9:12) ~ 1,
+      Condition %in% c(3, 5, 9, 11) & statement_number %in% c(1:4) ~ 1,
+      Condition %in% c(1, 6, 7, 12) & statement_number %in% c(5:8) ~ 1,
+      Condition %in% c(2, 4, 8, 10) & statement_number %in% c(9:12) ~ 1,
+      TRUE ~ 0
     )
-  )
-
-clean_data %>% 
-  group_by(
-    statement_accuracy
   ) %>% 
-  summarize(mean_resp = mean(response, na.rm = TRUE))
+  select(subject, ends_with("identifier"), contains("statement"), response, repeated, trial)
 
+clean_data <- rbind(clean_session1_data, clean_session2_data)
 
-write.csv(clean_data, paste0(script_dir, "./data/clean_data_2.csv"))
+write.csv(clean_data, paste0(script_dir, "./data/clean_data_3.csv"))
 
 statement_data <- clean_data %>% 
   select(contains("statement")) %>% 
   distinct()
 
-write.csv(statement_data, paste0(script_dir, "./data/statement_data_2.csv"))
+write.csv(statement_data, paste0(script_dir, "./data/statement_data_3.csv"))
 
 
 ## Exp4----
-data <- data.table::fread(paste0(script_dir, "./data/Pennycook et al._FakeNews_E4.csv"))
-
-clean_data <- session2_data %>% 
-  select(Warning, Condition, ends_with("_2")) %>% 
-  mutate(subject = row_number()) %>% 
-  pivot_longer(
-    cols = -c(Counterbalance, subject, Warning),
-    names_to = "item",
-    values_to = "response"
-  ) %>% 
-  mutate(
-    statement_identifier = str_extract(item, "^[A-Z][a-z]+\\d+"),
-    statement_text = NA,
-    statement_accuracy = case_when(
-      str_extract(item, "^[A-Z][a-z]+") == "Real" ~ 1,
-      str_extract(item, "^[A-Z][a-z]+") == "Fake" ~ 0,
-    ),
-    statement_number = as.numeric(str_extract(item, "\\d+")),
-    type = ifelse(str_detect(item, "RT"), "rt", "rating")
-  ) %>% 
-  filter(type != "rt") %>% 
-  mutate(
-    presentation_identifier = 1,
-    trial = NA,
-    between_identifier = 1,
-    repeated = case_when(
-      Counterbalance == 1 & statement_number < 7 ~ 1,
-      Counterbalance == 1 & statement_number > 6 ~ 0,
-      Counterbalance == 2 & statement_number < 7 ~ 0,
-      Counterbalance == 2 & statement_number > 6 ~ 1,
-    )
-  ) %>% 
-  # filter(!is.na(response)) %>% 
-  select(subject, ends_with("identifier"), contains("statement"), response, repeated, trial)
-
-
-write.csv(clean_data, paste0(script_dir, "./data/clean_data_2.csv"))
-
-statement_data <- clean_data %>% 
-  select(contains("statement")) %>% 
-  distinct()
-
-write.csv(statement_data, paste0(script_dir, "./data/statement_data_2.csv"))
+# Not real EXP
