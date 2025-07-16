266694921eee52f277ce175e291a25f5d6215a8d
Sven Lesche <113923517+SLesche@users.noreply.github.com>
2025-07-16 16:54:58 +0200

    werbu

--- example_submission_data/reading_complete_submissions.R
+++ example_submission_data/reading_complete_submissions.R
@@ -30,19 +30,6 @@ for (isubmission in seq_along(valid_files)){
   # inspect_study_data(submission_obj)
   # inspect_raw_data(submission_obj)[[1]] %>% count(certainty)
   
-  if (inspect_publication_data(submission_obj)$publication_code == "nadarevic_2018_foreign"){
-    # In this data, proportion_true was wrongfully entered with accuracy values
-    submission_obj$statementset_info[[1]]$statementset_data <- submission_obj$statementset_info[[1]]$statementset_data %>% 
-      mutate(
-        proportion_true = ifelse(statement_accuracy == 0, 1 - proportion_true, proportion_true)
-      )
-    
-    submission_obj$statementset_info[[2]]$statementset_data <- submission_obj$statementset_info[[2]]$statementset_data %>% 
-      mutate(
-        proportion_true = ifelse(statement_accuracy == 0, 1 - proportion_true, proportion_true)
-      )
-  }
-  
   add_submission_to_db(conn, submission_obj)
 }
 
