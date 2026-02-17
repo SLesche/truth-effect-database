511dcb324c4adb8c93b90ecf2cba057e915d8d69
Sven Lesche <113923517+SLesche@users.noreply.github.com>
2026-02-17 18:29:12 +0100

    add old statement set

--- example_submission_data/prep_submissions/stump2026stop/prep_data.R
+++ example_submission_data/prep_submissions/stump2026stop/prep_data.R
@@ -4,18 +4,16 @@ script_dir <- dirname(rstudioapi::getSourceEditorContext()$path)
 
 data <- readxl::read_excel(paste0(script_dir, "./data/summed_data_both_sessions_10_2025.xlsx")) 
 
-statement_data <- data %>%
-  distinct(statements) %>%
-  mutate(
-    statement_identifier = row_number(),
-    statement_text = statements,
-    statement_accuracy = NA
-  )
+old_statement_data <- read.csv(paste0(script_dir, "./../stump2022feeling/data/statement_data_1.csv"))
 
-write.csv(statement_data, paste0(script_dir, "./data/statement_data_1.csv"))
+clean_statement_data <- old_statement_data %>% 
+  mutate(statement_text = str_remove_all(statement_text, "\"")) %>% 
+  mutate(statement_text = str_replace(statement_text, "40.000", "40000")) %>% 
+  mutate(statement_text = str_replace(statement_text, "In den USA wird", "In USA wird"))
 
 clean_data <- data %>% 
-  left_join(statement_data) %>% 
+  mutate(statement_text = str_remove_all(statements, "'")) %>% 
+  left_join(clean_statement_data) %>% 
   mutate(
     procedure_identifier = session,
     within_identifier = 1,
@@ -27,7 +25,7 @@ clean_data <- data %>%
     trial = NA,
     confidence = confidence
   ) %>% 
-  select(subject, ends_with("identifier"), response, repeated, rt, confidence, trial) %>% 
+  # select(subject, ends_with("identifier"), response, repeated, rt, confidence, trial) %>% 
   filter(!is.na(subject))
 
 write.csv(clean_data, paste0(script_dir, "./data/clean_data_1.csv"))
